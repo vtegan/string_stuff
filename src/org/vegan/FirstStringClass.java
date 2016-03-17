53a9390bdba30f75a0f870258479a425ae7954f5
vtegan <vtegan@users.noreply.github.com>
2016-03-16 21:05:49 -0400

    Commit src dir.

---
+++ src/org/vegan/FirstStringClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vegan;
+
+/**
+ * @author egan
+ *
+ */
+public class FirstStringClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello.  Work with strings.");
+
+	}
+
+}
